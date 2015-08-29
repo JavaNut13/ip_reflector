4da9a6f7aa751da12c7b5436a6a7e26dd232c839
Will Richardson <william.hamish@gmail.com>
2015-08-29 17:27:38 +1200

    I think this will have fixed the formatting..?

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"golang.org/x/net/websocket"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"time"
-  "fmt"
 )
 
 const (
@@ -45,10 +45,10 @@ func WatchSocket(origin, url string, wait int, callback func([]byte)) {
 		if n, err = ws.Read(msg); err != nil {
 			log.Fatal(err)
 		} else {
-      if string(msg[:n]) != "{\"type\": \"nop\"}" {
-  			callback(msg)
-    		log.Printf("%s\n", msg[:n])
-      }
+			if string(msg[:n]) != "{\"type\": \"nop\"}" {
+				callback(msg)
+				log.Printf("%s\n", msg[:n])
+			}
 		}
 		time.Sleep(time.Duration(wait) * time.Second)
 	}
@@ -88,7 +88,7 @@ func DoPush(title, message, device, token string) {
 	client := &http.Client{}
 	body := "{\"type\": \"note\", \"title\": \"" + title + "\", \"body\": \"" + message + "\", \"device_iden\": \"" + device + "\"}"
 	req, _ := http.NewRequest("POST", PUSH_URL, bytes.NewBuffer([]byte(body)))
-	req.Header.Set("Authorization", "Bearer " + token)
+	req.Header.Set("Authorization", "Bearer "+token)
 	req.Header.Set("Content-Type", "application/json")
 	client.Do(req)
 }
@@ -98,26 +98,26 @@ func ReflectIp(pushes []Push, token string) {
 		if push.Title == "RPI" && push.Body == "reflect_ip" {
 			ip := GetIp()
 			DoPush("Reflected IP", ip, push.DeviceId, token)
-      break
+			break
 		}
 	}
 }
 
 func main() {
-  if len(os.Args) < 3 {
-    fmt.Println("Useage: reflect_ip <API key> <Sleep time (Seconds)>")
-    os.Exit(1)
-  }
-  token := os.Args[1]
-  waitStr := os.Args[2]
-  wait, err := strconv.ParseInt(waitStr, 10, 32)
-  if err != nil || wait < 0 {
-    fmt.Println("Sleep time must be a positive integer.")
-    os.Exit(2)
-  }
-  
-  call := func(b []byte) {
-    ReflectIp(GetPushes(token, int(wait)), token)
-  }
-  WatchSocket(ORIGIN, SOCKET + token, int(wait), call)
+	if len(os.Args) < 3 {
+		fmt.Println("Useage: reflect_ip <API key> <Sleep time (Seconds)>")
+		os.Exit(1)
+	}
+	token := os.Args[1]
+	waitStr := os.Args[2]
+	wait, err := strconv.ParseInt(waitStr, 10, 32)
+	if err != nil || wait < 0 {
+		fmt.Println("Sleep time must be a positive integer.")
+		os.Exit(2)
+	}
+
+	call := func(b []byte) {
+		ReflectIp(GetPushes(token, int(wait)), token)
+	}
+	WatchSocket(ORIGIN, SOCKET+token, int(wait), call)
 }
